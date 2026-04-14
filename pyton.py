ec4b14fe25e715bb79638a010231bb8b6a140a8b
Ashish br <ashishbr@gameberrylabs.com>
2026-04-14 20:26:27 +0530

    change1

---
+++ pyton.py
@@ -0,0 +1,9 @@
+setattr(obj, 'attribute_name', 'value')
+setattr(obj, 'attribute_name', value)
+# Example
+class MyClass:
+    pass            
+obj = MyClass()
+nfjskl = 'Hello, World!'
+setattr(obj, 'greeting', nfjskl)
+print(obj.greeting)  # Output: Hello, World!
